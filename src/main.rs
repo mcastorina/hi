afc04732963dd33be3969c6f0773d4514c67ac3e
Miccah Castorina <m.castorina93@gmail.com>
2020-02-15 21:36:25 -0600

    Read stdin and color to bright red

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io::{self, Read};
+use colored::*;
+
 fn main() {
-    println!("Hello, world!");
+    print!("{}", read_stdin().bold().bright_red());
+}
+
+fn read_stdin() -> String {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer).unwrap();
+    buffer
 }
